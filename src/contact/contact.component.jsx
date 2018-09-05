e6d1c688ae5fba1b3b6d58d5292085c6c823d896
a.ilic <a.ilic@levi9.com>
2018-09-05 13:26:05 +0200

    TODO

--- src/contact/contact.component.jsx
+++ src/contact/contact.component.jsx
@@ -38,16 +38,16 @@ export default class Education extends React.Component {
                 </div>
                 <div className="right-section">
                     <Reveal effect="animated slideInUp">
-                        <a href="mailto:rjtm1992@gmail.com" className="mail">
-                            rjtm1992@gmail.com
+                        <a href="mailto:alexilic92@gmail.com" className="mail">
+                            alexilic92@gmail.com
                         </a>
-                        <a href="https://www.linkedin.com/in/rjtm1992" target="_blank">
+                        <a href="https://www.linkedin.com/in/aleksandar-ilić-a9b30b87/" target="_blank">
                             <img src={require('./linkedin.png')} alt="LinkedIn"/>
                         </a>
-                        <a href="https://github.com/rjtm" target="_blank">
+                        <a href="https://github.com/acailic" target="_blank">
                             <img src={require('./github.png')} alt="Github"/>
                         </a>
-                        <a href="/resume.pdf" className="pdf-download" download="RicardoMontero.pdf">
+                        <a href="/resume.pdf" className="pdf-download" download="IlicAleksandar.pdf">
                             <i className="material-icons">file_download</i>
                         </a>
                     </Reveal>
@@ -56,7 +56,7 @@ export default class Education extends React.Component {
                     last updated { this.state.lastCommit.date } <a href={this.state.lastCommit.url} target="_blank">({this.state.lastCommit.sha})</a>
                 </div>
                 <div className="bottom-text">
-                    Ricardo Montero 2017
+                    Ilic Aleksandar 2017
                 </div>
             </Element>
         );
