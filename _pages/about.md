a8ffc57c3a4cdcd59344c9c8b99147e24785b918
aleksandarilic <a.ilic@vegait.rs>
2021-07-11 00:52:53 +0200

    new io

---
+++ _pages/about.md
@@ -0,0 +1,9 @@
+---
+title: "About"
+permalink: /about/
+header:
+  image: "/images/about-header.jpg"
+---
+{: .text-left}
+having fun.
+{: .text-center}
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,26 @@
+var store = [{
+        "title": "Data processing with Akka Actors: Part I",
+        "excerpt":"Everyone experienced in a field of distributed and data-intensive systems or big data technologies,in general, must have heard about Akka.This article will be part of the series, where I will try to introduce you to the actor programming model while developing a data processing application exploiting techniques provided by Akka...","categories": [],
+        "tags": ["scala","akka","jvm","big data"],
+        "url": "http://localhost:4000/akka-actors-1/",
+        "teaser":null},{
+        "title": "Data processing with Akka Actors: Part II",
+        "excerpt":"In the first article in the Data processing with Akka Actors series, I have introduced a problem that we are trying to solve. There was some talk about actors and how to organize them into an actor hierarchy. Then we got deep into details while implementing two simple actors with...","categories": [],
+        "tags": ["scala","akka","jvm","big data"],
+        "url": "http://localhost:4000/akka-actors-2/",
+        "teaser":null},{
+        "title": "The Power of Akka Streams",
+        "excerpt":"In the first and the second part of the Data processing with Akka Actors series, I introduced how to design actors based application. The example that I presented is a way to simple and using actors to solve that is obliviously an overkill, but I think it’s a representative enough...","categories": [],
+        "tags": ["scala","akka","jvm","big data","akka streams"],
+        "url": "http://localhost:4000/power-of-akka-streams/",
+        "teaser":null},{
+        "title": "A Taste of Functional Java",
+        "excerpt":"There are lots of different definitions of what functional programming is. For people new to it, it can be really hard to understand what it actually is, since there is a plethora of materials related to it. You will rarely find one that will clearly present basics and give you...","categories": [],
+        "tags": ["java","jvm","fp"],
+        "url": "http://localhost:4000/a-taste-of-functional-java/",
+        "teaser":null},{
+        "title": "Functional Effects with ZIO",
+        "excerpt":"ZIO 1.0 is finally released after three years of active development and more than 300 contributors, so I decided to write a practical introduction to ZIO and functional effects.In my last article, I was talking about some basic concepts related to functional programming.We already know that Java is not a...","categories": [],
+        "tags": ["scala","jvm","zio","fp"],
+        "url": "http://localhost:4000/functional-effects-with-zio/",
+        "teaser":null}]
